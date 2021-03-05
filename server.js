f6f4bf4303abb41ed96b2e5f9341dc07bb84a3ee
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-05 16:49:55 +1300

    production json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Define Routes
+// Define Routes 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
