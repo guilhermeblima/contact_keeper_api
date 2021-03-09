c930e37db7baa3e89bf8bac72827f26886b9c7cd
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-10 11:18:59 +1300

    cords

--- server.js
+++ server.js
@@ -16,4 +16,10 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
+// enable CORS without external module
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
