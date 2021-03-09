cf3fb7720adc84205157a0f81c510de2eb2ccd37
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-10 11:29:57 +1300

    cords

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// enable CORS without external module
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
@@ -16,10 +22,5 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-// enable CORS without external module
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
